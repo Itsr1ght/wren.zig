8da88f38144ff70e3f7a94a9dcf0a9f1b6f97d64
Bilal <bjlegend007@gmail.com>
2026-09-25 22:08:56 +0530

    Changes:
    * Added multi-string support and its test
    * Check for reserve keyword
    * Check for identifier

--- src/compiler/Lexer.zig
+++ src/compiler/Lexer.zig
@@ -1,6 +1,5 @@
 pub const TokenType = enum {
     // Reserved words
-    as,
     @"break",
     class,
     construct,
@@ -11,6 +10,7 @@ pub const TokenType = enum {
     foreign,
     @"if",
     import,
+    as,
     in,
     is,
     null,
@@ -56,8 +56,8 @@ pub const TokenType = enum {
     doublequote, // "
     underscore, // _
     // Types
+    identifier,
     number,
-    character,
     string,
     boolean,
     eof,
@@ -77,6 +77,36 @@ line: usize = 0,
 
 fn isDigit(c: u8) bool {
     return c >= '0' and c <= '9';
+}
+
+fn isAlpha(c: u8) bool {
+    return (c >= 'a' and c <= 'z') or (c >= 'A' and c <= 'Z') or c == '_';
+}
+
+fn identiferType(text: []const u8) TokenType {
+    const keywords = std.StaticStringMap(TokenType).initComptime(.{
+        .{ "if", .@"if" },
+        .{ "else", .@"else" },
+        .{ "var", .@"var" },
+        .{ "while", .@"while" },
+        .{ "break", .@"break" },
+        .{ "continue", .@"continue" },
+        .{ "for", .@"for" },
+        .{ "return", .@"return" },
+        .{ "class", .class },
+        .{ "super", .super },
+        .{ "this", .this },
+        .{ "true", .true },
+        .{ "false", .false },
+        .{ "null", .null },
+        .{ "foreign", .foreign },
+        .{ "import", .import },
+        .{ "static", .static },
+        .{ "as", .as },
+        .{ "in", .in },
+        .{ "is", .is },
+    });
+    return keywords.get(text) orelse .identifier;
 }
 
 pub fn init(source: []const u8) Self {
@@ -138,6 +168,42 @@ fn number(self: *Self) Token {
     };
 }
 
+fn string(self: *Self) !Token {
+    _ = self.advance();
+    const start = self.pos;
+    while (self.peek() != '"' and !self.isAtEnd()) {
+        if (self.peek() == '\n') self.line += 1;
+        _ = self.advance();
+    }
+    const length = self.pos - start;
+    if (self.isAtEnd()) return error.StringEnd;
+
+    _ = self.advance();
+
+    return .{
+        .token_type = .string,
+        .start = self.data.ptr + start,
+        .length = length,
+        .line = self.line,
+    };
+}
+
+fn identifier(self: *Self) Token {
+    const start = self.pos;
+
+    while (isAlpha(self.peek()) or isDigit(self.peek())) {
+        _ = self.advance();
+    }
+
+    const text = self.data[start..self.pos];
+    return .{
+        .token_type = identiferType(text),
+        .start = self.data.ptr + start,
+        .length = self.pos - start,
+        .line = self.line,
+    };
+}
+
 pub fn nextToken(self: *Self) !Token {
     self.skipWhiteSpace();
     if (self.isAtEnd()) {
@@ -152,6 +218,10 @@ pub fn nextToken(self: *Self) !Token {
     const c = self.peek();
     if (isDigit(c)) {
         return self.number();
+    }
+    switch (c) {
+        '\"' => return try self.string(),
+        else => return self.identifier(),
     }
 
     return error.UnHandledCharacter;
@@ -173,4 +243,45 @@ test "Parse a Float" {
 
     try std.testing.expectEqual(token.token_type, TokenType.number);
     try std.testing.expectEqualStrings("3.14", token.start[0..token.length]);
+}
+
+test "Parse a String" {
+    var lexer = Self.init("\"hello\"");
+    const token = try lexer.nextToken();
+
+    try std.testing.expectEqual(token.token_type, TokenType.string);
+    try std.testing.expectEqualStrings("hello", token.start[0..token.length]);
+}
+
+test "Parse a multi-line String" {
+    var lexer = Self.init(
+        \\"
+        \\ Hello World
+        \\ Snap back to reality
+        \\"
+    );
+    const token = try lexer.nextToken();
+
+    try std.testing.expectEqual(token.token_type, TokenType.string);
+    try std.testing.expectEqualStrings(
+        \\
+        \\ Hello World
+        \\ Snap back to reality
+        \\
+    , token.start[0..token.length]);
+}
+
+test "Parse a while" {
+    var lexer = Self.init("while");
+    const token = try lexer.nextToken();
+
+    try std.testing.expectEqual(token.token_type, TokenType.@"while");
+}
+
+test "Parse a identifier" {
+    var lexer = Self.init("hello_world");
+    const token = try lexer.nextToken();
+
+    try std.testing.expectEqual(token.token_type, TokenType.identifier);
+    try std.testing.expectEqualStrings("hello_world", token.start[0..token.length]);
 }
